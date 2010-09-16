83acfb973bd7cfda016071afb5dcf02cde87b3e5
Alex Bepple <alex@bepple.de>
2010-09-16 16:04:16 +0200

    REST-Server mit Play hinzugefügt.

---
+++ server/app/controllers/Kalender.java
@@ -0,0 +1,10 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public class Kalender extends Controller {
+
+    public static void alleTermine() {
+        render();
+    }
+}
--- spec/src/main/java/de/itagile/restlendar/spec/Kalender.java
+++ spec/src/main/java/de/itagile/restlendar/spec/Kalender.java
@@ -1,11 +1,44 @@
 package de.itagile.restlendar.spec;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.GetMethod;
+
 public class Kalender {
 
-    public List holeTermine() {
-        return Arrays.asList();
+    private String basisUrl;
+    private String kalenderRessource = "kalender";
+
+    public Kalender(String basisUrl) {
+        this.basisUrl = basisUrl;
+    }
+    
+    public List<String> holeTermine() throws Exception {
+        return entferneLeere(einzeltermineAus(alleTermine()));
+    }
+
+    private String alleTermine() throws Exception {
+        HttpClient httpClient = new HttpClient();
+        GetMethod getMethod = new GetMethod(String.format("%s/%s", basisUrl, kalenderRessource));
+        String termine = "";
+        try {
+            httpClient.executeMethod(getMethod);
+            termine = getMethod.getResponseBodyAsString();
+        } finally {
+            getMethod.releaseConnection();
+        }
+        return termine;
+    }
+
+    private List<String> entferneLeere(List<String> result) {
+        result.remove("");
+        return result;
+    }
+
+    private List<String> einzeltermineAus(String response) {
+        return new ArrayList<String>(Arrays.asList(response.split("\\n")));
     }
 }
