580e9eeb6b70f5b7cf5d341f10a57ffbfebc6218
Alex Bepple <alex@bepple.de>
2010-09-16 12:35:26 +0200

    Erster Testfall & Infrastruktur

---
+++ src/main/java/de/itagile/restlendar/spec/Kalender.java
@@ -0,0 +1,11 @@
+package de.itagile.restlendar.spec;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Kalender {
+
+    public List holeTermine() {
+        return Arrays.asList();
+    }
+}
